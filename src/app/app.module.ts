709a6e1c1c3906d7f2ffb0bbe499a716faf4063b
mateuszfranke <mateuszfranke@icloud.com>
2020-11-23 10:58:02 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { InformationsComponent } from './informations/informations.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsComponent,
+    InformationsComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule
